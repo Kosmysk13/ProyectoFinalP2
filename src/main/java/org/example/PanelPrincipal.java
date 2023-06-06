e618ca570ffb36f3829ab4d22fea9c5a52a04220
Kosmysk13 <durbano2020@udec.cl>
2023-06-06 17:55:57 -0400

    Se ajustan paneles de asientos, horarios y botones menu de los recorridos. Se agrega boton para simular pago

--- src/main/java/org/example/PanelPrincipal.java
+++ src/main/java/org/example/PanelPrincipal.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.concurrent.CompletableFuture;
 
 /**
  * Crea el panel que contendra al panel comprador y panel expendedor en su interior, se encarga de juntar ambos
@@ -11,14 +12,17 @@ import java.awt.event.ActionListener;
  */
 public class PanelPrincipal extends JPanel implements ActionListener{
     private JComboBox origen,destino;
-    private JButton busqueda;
+    private JButton busqueda,ConfirmarPago;
+    private JLabel textAsientos,textHorarios;
     private String[] ciudadOrigen,ciudadDestino;
+    private String auxrec;
+    private Icon a;
     /**
      * Metodo constructor que crea instancias del panel comprador y panel expendedor
      * Ademas define los limites del panel
      */
     public PanelPrincipal(){
-        this.setLayout(new FlowLayout());
+        this.setLayout(null);
         this.setBackground(new Color(188,227,255));
         this.setVisible(true);
 
@@ -26,17 +30,34 @@ public class PanelPrincipal extends JPanel implements ActionListener{
         ciudadDestino = new String[] {String.valueOf(Recorridos.SANTIAGO),String.valueOf(Recorridos.CONCEPCION),String.valueOf(Recorridos.CHILLAN)};
         origen = new JComboBox(ciudadOrigen);
         origen.addActionListener(this);
+        origen.setBounds(100,50,300,50);
+        origen.setFocusable(false);
         this.add(origen);
         destino = new JComboBox(ciudadDestino);
         destino.addActionListener(this);
+        destino.setBounds(450,50,300,50);
+        destino.setFocusable(false);
         this.add(destino);
 
-        busqueda = new JButton();
-        busqueda.setBounds(700,10,25,25);
+        a = new ImageIcon("lupa.png");
+        busqueda = new JButton(a);
+        busqueda.setBounds(800,50,50,50);
+        busqueda.setIcon(a);
         busqueda.addActionListener(this);
-        busqueda.setFocusable(false);
-        busqueda.setIcon(new ImageIcon("lupa.png"));
         this.add(busqueda);
+
+        ConfirmarPago = new JButton("Confirmar Pago");
+        ConfirmarPago.setBounds(1000,750,200,50);
+        ConfirmarPago.setFocusable(false);
+        this.add(ConfirmarPago);
+
+        textAsientos = new JLabel("Asientos");
+        textAsientos.setBounds(800,324,100,50);
+        this.add(textAsientos);
+
+        textHorarios = new JLabel("Horarios");
+        textHorarios.setBounds(175,324,100,50);
+        this.add(textHorarios);
     }
 
     /**
@@ -46,18 +67,25 @@ public class PanelPrincipal extends JPanel implements ActionListener{
     public void paint (Graphics g){
         super.paint(g);
         g.setColor(Color.white);
-        g.fillRect(500,324, 700, 600);                               //Panel Asientos
+        g.fillRect(500,374, 700, 350);                               //Panel Asientos
         g.setColor(Color.black);
-        g.drawRect(500,324, 700, 600);                               //Panel asientos Borde
+        g.drawRect(500,324, 700, 400);                               //Panel asientos Borde
+        g.drawLine(500,374,1200,374);
         g.setColor(Color.white);
-        g.fillRect(50,324, 300, 600);                               //Panel Horarios
+        g.fillRect(50,374, 300, 350);                               //Panel Horarios
         g.setColor(Color.black);
-        g.drawRect(50,324, 300, 600);                               //Panel Horarios Borde
+        g.drawRect(50,324, 300, 400);                               //Panel Horarios Borde
+        g.drawLine(50,374,350,374);
+
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource()==origen){
+            if (auxrec!=null){
+                destino.addItem(auxrec);
+            }
+            auxrec = origen.getSelectedItem().toString();
             destino.removeItem(origen.getSelectedItem());
         }
     }
